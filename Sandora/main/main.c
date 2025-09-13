cdbf58496a249c5fc157a8200aaa9721be38626d
sakthiasthik <sakthieee1996@gmail.com>
2025-09-13 21:33:47 +0530

    i have deleted the hour glass logic files

--- Sandora/main/main.c
+++ Sandora/main/main.c
@@ -1,50 +1,23 @@
-// #include "mpu6050.h"
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/task.h"
-
-// #include "max7219_led.h"
-
-// static const char *TAG = "MAIN";
-
-
-// void app_main(void) 
-// {
-//     // if (mpu6050_init() == ESP_OK) 
-//     // {
-//     //     mpu6050_test();  // Runs infinite loop printing processed orientation
-//     //     vTaskDelay(pdMS_TO_TICKS(500));
-//     // }      
-//     max7219_init();
-//     while (1) 
-//     {
-//         max7219_test_pattern();
-//         vTaskDelay(pdMS_TO_TICKS(2000));
-//     }
-// }
-
-
-
-
-
-
-
 #include "mpu6050.h"
-#include "max7219_led.h"
-#include "hourglass.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "max7219_led.h"
+
+static const char *TAG = "MAIN";
+
+
 void app_main(void) 
 {
-    mpu6050_init();
+    // if (mpu6050_init() == ESP_OK) 
+    // {
+    //     mpu6050_test();  // Runs infinite loop printing processed orientation
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    // }      
     max7219_init();
-    hourglass_init(600000); // 10 min
-
     while (1) 
     {
-        // Orientation_t o = mpu6050_get_orientation_state();
-        Orientation_t o = MPU6050_GetOrientation();
-        hourglass_update(o);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        max7219_test_pattern();
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
